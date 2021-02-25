c12e98deeb7dce6659cad157e8decb34fb372b54
Taki <takieyamani@gmail.com>
2021-02-25 12:07:46 +0100

    fixed password hashing bug

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -8,13 +8,14 @@ export class Password {
     const salt = randomBytes(8).toString('hex');
     const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
 
-    return `${buffer.toString('hex')}.${salt}}`;
+    return `${buffer.toString('hex')}.${salt}`;
   }
 
   static async compare(password: string, incomingPassword: string) {
     const [hashedPassword, salt] = password.split('.');
-    const buffer = (await scryptAsync(incomingPassword, salt, 64)) as Buffer;
 
-    return buffer.toString('hex') === password;
+    const buffer = (await scryptAsync(incomingPassword, salt, 64)) as Buffer;
+    // console.log(buffer.toString('hex'), 'hashed', hashedPassword);
+    return buffer.toString('hex') === hashedPassword;
   }
 }
